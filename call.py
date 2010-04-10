e1be0165d8bf6fbc6266245fb319d52c10fa56fa
Jesse Andrews <anotherjesse@gmail.com>
2010-04-09 17:41:05 -0700

    amqp based run and describe (and other api methods) probably works

---
+++ call.py
@@ -0,0 +1,35 @@
+import os
+import sys
+import uuid
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.split(__file__)[0], 'contrib')))
+
+import anyjson
+
+from carrot import connection
+from carrot import messaging
+
+conn = connection.BrokerConnection(hostname="localhost", port=5672,
+                                   userid="guest", password="guest",
+                                   virtual_host="/")
+
+msg_id = uuid.uuid4().hex
+msg = anyjson.deserialize(sys.argv[2])
+msg.update({'_msg_id': msg_id})
+print msg
+
+
+def response(message_data, message):
+    print "response", message_data
+    message.ack()
+    sys.exit(0)
+
+consumer = messaging.Consumer(connection=conn, queue=msg_id, exchange=msg_id)
+consumer.register_callback(response)
+
+publisher = messaging.Publisher(connection=conn, queue=sys.argv[1], exchange=sys.argv[1])
+publisher.send(msg)
+publisher.close()
+
+consumer.wait()
+
---
+++ node_worker.py
@@ -0,0 +1,39 @@
+import os
+import sys
+import node
+import logging
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.split(__file__)[0], 'contrib')))
+
+from carrot import connection
+from carrot import messaging 
+
+n = node.Node()
+
+conn = connection.BrokerConnection(hostname="localhost", port=5672,
+                                   userid="guest", password="guest",
+                                   virtual_host="/")
+
+def router(message_data, message):
+    print "Got message: %s" % message_data
+
+    try:
+        msg_id = message_data.pop('_msg_id')
+    except Exception:
+        logging.exception("ouch")
+        message.ack()
+        return
+
+    print "foobar"
+    rval = getattr(n, message_data.get('method'))(**dict([(str(k), v) for k, v in message_data.get('args', {}).iteritems()]))
+    print "Trying to send: %s" % rval
+
+    publisher = messaging.Publisher(connection=conn, queue=msg_id, exchange=msg_id)
+    publisher.send({'result': rval})
+    publisher.close()
+    message.ack()
+
+consumer = messaging.Consumer(connection=conn, queue="node", exchange="node")
+consumer.register_callback(router)
+consumer.wait()
+
