39bc15b8f52aa41a16c27f46b0fe0e251cf989de
andy <git@term.ie>
2010-04-10 15:52:34 -0700

    moved fake virt implementation into its own file

--- node.py
+++ node.py
@@ -11,6 +11,7 @@ from xml.etree import ElementTree
 import libvirt
 
 import exception
+import fakevirt
 import partition2disk
 import settings
 import storage
@@ -18,58 +19,6 @@ from utils import runthis
 
 import calllib
 
-class FakeVirtConnection(object):
-    def __init__(self, options=None):
-        self.options = options
-        self.next_index = 0
-        self.instances = {}
-
-    def lookupByID(self, i):
-        return self.instances[str(i)]
-
-    def listDomainsID(self):
-        return self.instances.keys()
-
-    def lookupByName(self, instance_id):
-        for x in self.instances.values():
-            if x.name() == instance_id:
-                return x
-        raise Exception('no instance found for instance_id: %s' % instance_id)
-
-    def createXML(self, xml, flags):
-        # parse the xml :(
-        xml_stringio = StringIO.StringIO(xml)
-
-        my_xml = ElementTree.parse(xml_stringio)
-        name = my_xml.find('name').text
-        logging.info('name %s', name)
-
-        fake_instance = FakeVirtInstance(conn=self, 
-                                         index=str(self.next_index),
-                                         name=name,
-                                         xml=my_xml)
-        self.instances[str(self.next_index)] = fake_instance
-        self.next_index += 1
-
-    def _removeInstance(self, i):
-        self.instances.pop(str(i))
-
-
-class FakeVirtInstance(object):
-    def __init__(self, conn, index, name, xml):
-        self._conn = conn
-        self._destroyed = False
-        self._name = name
-        self._index = index
-
-    def name(self):
-        return self._name
-
-    def destroy(self):
-        if self._destroyed:
-            raise Exception('instance already destroyed: %s' % self.name())
-        self._destroyed = True
-        self._conn._removeInstance(self._index)
 
 
 class Node(object):
@@ -84,7 +33,7 @@ class Node(object):
         # TODO(termie): maybe lazy load after initial check for permissions
         # TODO(termie): check whether we can be disconnected
         if options and options.use_fake:
-            conn = FakeVirtConnection(options)
+            conn = fakevirt.FakeVirtConnection(options)
         else:
             auth = [[libvirt.VIR_CRED_AUTHNAME, libvirt.VIR_CRED_NOECHOPROMPT], 
                     'root',
