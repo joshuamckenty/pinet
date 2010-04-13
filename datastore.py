dfdffe376a8ac45d636d150bc223d2e32f42c1da
Joshua McKenty <jmckenty@dancelamb.(none)>
2010-04-12 17:11:08 -0700

    Adding a trivial file-based KVS. (untested)

---
+++ datastore.py
@@ -0,0 +1,35 @@
+import os
+import system
+import contrib
+import anyjson
+
+import flags
+
+FLAGS = flags.FLAGS
+
+flags.DEFINE_string('datastore_path', '/root/pinet/keeper',
+                    'where keys are stored on disk')
+
+class keeper(Object):
+    def __init__(self, prefix="pinet-"):
+        self.prefix = prefix
+        self._path = FLAGS.datastore_path
+
+    def _slugify(self, key):
+        return key
+
+    def __getattr__(self, item):
+        # TODO - Memoize this
+        item = self._slugify(item)
+        path = "%s/%s%s" % (self._path, self.prefix, item)
+        if os.path.isfile(path):
+            return anyjson.deserialize(open(path, 'r').read())
+        return None
+
+    def __setattr__(self, item, value):
+        item = self._slugify(item)
+        path = "%s/%s%s" % (self._path, self.prefix, item)
+        f = open(path, "w")
+        f.write(anyjson.serialize(value))
+        # TODO: Pop and return the old value?
+        return value
