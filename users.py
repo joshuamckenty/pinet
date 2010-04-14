d9911faad23527fd96954d8b3400a858ee2d1d12
Vishvananda Ishaya <vishvananda@yahoo.com>
2010-04-14 13:51:31 -0700

    users refactoring and tests update

--- users.py
+++ users.py
@@ -27,7 +27,6 @@ flags.DEFINE_string('ldap_subtree', 'ou=Users,dc=example,dc=com', 'OU for Users'
 
 from M2Crypto import RSA, BIO
 
-
 def _generate_key_pair(bits=1024):
     key = RSA.gen_key(bits, 65537)
     bio = BIO.MemoryBuffer()
@@ -41,79 +40,91 @@ class LdapUserException(Exception):
     def __str__(self):
         return self.message
 
+class User:
+    def __init__(self, manager, ldap_user_object):
+        self.manager = manager
+        self.ldap_user_object = ldap_user_object
+
+    @property
+    def id(self):
+        return self.ldap_user_object['uid'][0]
+
+    @property
+    def access(self):
+        return self.ldap_user_object['accessKey'][0]
+
+    @property
+    def secret(self):
+        return self.ldap_user_object['secretKey'][0]
+    
+    def create_key_pair(self, name):
+        return self.manager.create_key_pair(self.id, name)
+
+    def create_public_key(self, name, public_key):
+        return self.manager.create_public_key(self.id, name, public_key)
+
+    def get_public_key(self, name):
+        return self.manager.get_public_key(self.id, name)
+
+    def delete_public_key(self, name):
+        return self.manager.delete_public_key(self.id, name)
+
 class UserManager:
     def __init__(self):
         if FLAGS.fake_users:
-            self.create('fake', 'fake', 'fake')
+            self.create_user('fake', 'fake', 'fake')
 
     def authenticate(self, params, signature, verb='GET', server_string='127.0.0.1:8773', path='/'):
         # TODO: Check for valid timestamp
         access_key = params['AWSAccessKeyId']
-        with LDAPWrapper() as conn:
-            user = conn.find_user_by_access_key(access_key)
-        
+        user = self.get_user_from_access_key(access_key)
         if user == None:
             return None
-        secret_key = user['secretKey'][0]
-        expected_signature = signer.Signer(secret_key).generate(params, verb, server_string, path)
+        expected_signature = signer.Signer(user.secret).generate(params, verb, server_string, path)
         if signature == expected_signature:
             return user
-        
-    def keys(self, name):
-        """ return access & secret for a username """
-        with LDAPWrapper() as conn:
-            return conn.get_user_keys(name)
-    
-    def get_secret_from_access(self, access_key):
-        """ retreive the secret key for a given access key """
-        with LDAPWrapper() as conn:
-            user = conn.find_user_by_access_key(access_key)
-            if user:
-                return user['secretKey'][0]
-            return False
 
-    def create(self, name, access=None, secret=None):
+    def get_user(self, name):
+        with LDAPWrapper() as conn:
+            ldap_user_object = conn.find_user(name)
+        if ldap_user_object == None:
+            return None
+        return User(self, ldap_user_object)
+    
+    def get_user_from_access_key(self, access_key):
+        with LDAPWrapper() as conn:
+            ldap_user_object = conn.find_user_by_access_key(access_key)
+        if ldap_user_object == None:
+            return None
+        return User(self, ldap_user_object)
+
+    def create_user(self, uid, access=None, secret=None):
         if access == None: access = str(uuid.uuid4())
         if secret == None: secret = str(uuid.uuid4())
         with LDAPWrapper() as conn:
-            conn.create_user(name, access, secret)
+            conn.create_user(uid, access, secret)
 
-    def delete(self, name):
+    def delete_user(self, uid):
         with LDAPWrapper() as conn:
-            conn.delete_user(name)
+            conn.delete_user(uid)
 
-    def get_user_name_from_access(self, access_key):
-        with LDAPWrapper() as conn:
-            user = conn.find_user_by_access_key(access_key)
-        if user == None:
-            return None
-        return user['uid'][0]
-
-    def create_key_pair_from_access(self, access_key, key_name): 
-        user_name = self.get_user_name_from_access(access_key)
-        return self.create_key_pair(user_name, key_name)
-
-    def create_key_pair(self, user_name, key_name):
+    def create_key_pair(self, uid, key_name):
         private_key, public_key = _generate_key_pair()
-        with LDAPWrapper() as conn:
-            conn.create_public_key(user_name, key_name, public_key)
+        self.create_public_key(uid, key_name, public_key)
         return private_key
+        
 
-    def get_public_key_from_access(self, access_key, key_name): 
-        user_name = self.get_user_name_from_access(access_key)
-        return self.get_public_key(user_name, key_name)
-
-    def get_public_key(self, user_name, key_name):
+    def create_public_key(self, uid, key_name, public_key):
         with LDAPWrapper() as conn:
-            return conn.get_public_key(user_name, key_name)
+            conn.create_public_key(uid, key_name, public_key)
+
+    def get_public_key(self, uid, key_name):
+        with LDAPWrapper() as conn:
+            return conn.get_public_key(uid, key_name)
     
-    def delete_key_pair_from_access(self, access_key, key_name): 
-        user_name = self.get_user_name_from_access(access_key)
-        self.delete_key_pair(user_name, key_name)
-
-    def delete_key_pair(self, user_name, key_name):
+    def delete_public_key(self, uid, key_name):
         with LDAPWrapper() as conn:
-            conn.delete_public_key(user_name, key_name)
+            conn.delete_public_key(uid, key_name)
 
 class LDAPWrapper(object):
     def __init__(self):
@@ -157,22 +168,22 @@ class LDAPWrapper(object):
     def user_exists(self, name):
         return self.find_user(name) != None
         
-    def find_public_key(self, user_name, key_name):
+    def find_public_key(self, uid, key_name):
         dn = 'cn=%s,uid=%s,%s' % (key_name,
-                                   user_name,
+                                   uid,
                                    FLAGS.ldap_subtree)
         return self.find_object(dn, '(objectclass=pinetPublicKey)')
 
-    def delete_public_keys(self, user_name):
-        dn = 'uid=%s,%s' % (user_name, FLAGS.ldap_subtree)
+    def delete_public_keys(self, uid):
+        dn = 'uid=%s,%s' % (uid, FLAGS.ldap_subtree)
         key_objects = self.find_objects(dn, '(objectclass=pinetPublicKey)')
         if key_objects != None:
             for key_object in key_objects:
                 key_name = key_object[1]['cn'][0]
-                self.delete_public_key(user_name, key_name)
+                self.delete_public_key(uid, key_name)
 
-    def public_key_exists(self, user_name, key_name):
-        return self.find_public_key(user_name, key_name) != None
+    def public_key_exists(self, uid, key_name):
+        return self.find_public_key(uid, key_name) != None
         
     def create_user(self, name, access_key, secret_key):
         if self.user_exists(name):
@@ -192,24 +203,24 @@ class LDAPWrapper(object):
         self.conn.add_s('uid=%s,%s' % (name, FLAGS.ldap_subtree),
                         attr)
 
-    def create_public_key(self, user_name, key_name, public_key):
+    def create_public_key(self, uid, key_name, public_key):
         """create's a public key in the directory underneath the user"""
         # TODO(vish): possibly refactor this to store keys in their own ou
         #   and put dn reference in the user object
-        if not self.user_exists(user_name):
-            raise LdapUserException("User " + user_name + " doesn't exist")
-        if self.public_key_exists(user_name, key_name):
+        if not self.user_exists(uid):
+            raise LdapUserException("User " + uid + " doesn't exist")
+        if self.public_key_exists(uid, key_name):
             raise LdapUserException("Public Key " +
                                     key_name +
                                     " already exists for user " +
-                                    user_name)
+                                    uid)
         attr = [
             ('objectclass', ['pinetPublicKey']),
             ('cn', key_name),
             ('sshPublicKey', public_key),
         ]
         self.conn.add_s('cn=%s,uid=%s,%s' % (key_name,
-                                             user_name,
+                                             uid,
                                              FLAGS.ldap_subtree),
                                              attr)
     
@@ -224,14 +235,14 @@ class LDAPWrapper(object):
             return None
         return res[0][1] # return attribute list
 
-    def get_public_key(self, user_name, key_name):
-        if not self.public_key_exists(user_name, key_name):
+    def get_public_key(self, uid, key_name):
+        if not self.public_key_exists(uid, key_name):
             raise LdapUserException("Public Key " +
                                     key_name +
                                     " doesn't exist for user " +
-                                    user_name)
+                                    uid)
 
-        attr = self.find_public_key(user_name, key_name)
+        attr = self.find_public_key(uid, key_name)
         return (attr['sshPublicKey'][0])
 
     def get_user_keys(self, name):
@@ -242,13 +253,13 @@ class LDAPWrapper(object):
         return (attr['accessKey'][0],
                 attr['secretKey'][0])
 
-    def delete_public_key(self, user_name, key_name):
-        if not self.public_key_exists(user_name, key_name):
+    def delete_public_key(self, uid, key_name):
+        if not self.public_key_exists(uid, key_name):
             raise LdapUserException("Public Key " +
                                     key_name +
                                     " doesn't exist for user " +
-                                    user_name)
-        self.conn.delete_s('cn=%s,uid=%s,%s' % (key_name, user_name,
+                                    uid)
+        self.conn.delete_s('cn=%s,uid=%s,%s' % (key_name, uid,
                                           FLAGS.ldap_subtree))
         
 
@@ -265,20 +276,6 @@ def usage():
     print 'usage: %s -c username (access_key) (secret_key) | -d username' % sys.argv[0]
 
 if __name__ == "__main__":
-    # um = UserManager({'use_fake': True})
-    # um.create('jesse')
-    # access, actual_secret = um.keys('jesse')
-    # secret = um.get_secret_from_access(access)
-    # assert(secret == actual_secret)
-    # assert(None == um.get_secret_from_access('asdf'))
-    # um.delete('jesse')
-    # print 'works!'
-    # # assert(um.user_exists('jesse'))
-    # # um.delete('jesse')
-    # 
-    # 
-    # sys.exit(0)
-    
     manager = UserManager()
     
     if len(sys.argv) > 2:
