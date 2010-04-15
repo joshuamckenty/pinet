778e0e116a6a4da5e9fc1dd6df649814c8228ca5
andy <github@anarkystic.com>
2010-04-15 14:39:58 -0700

    actually make the loop work this time

--- calllib.py
+++ calllib.py
@@ -43,15 +43,6 @@ class Connection(connection.BrokerConnection):
         return cls._instance
 
 
-class StoppableHandler(object):
-    def __init__(self, fd, io_inst):
-        self.fd = fd
-        self.io_inst = io_inst
-
-    def stop(self):
-        self.io_inst.remove_handler(self.fd)
-
-
 class Consumer(messaging.Consumer):
     # TODO(termie): it would be nice to give these some way of automatically
     #               cleaning up after themselves
@@ -59,24 +50,13 @@ class Consumer(messaging.Consumer):
         if io_inst is None:
             io_inst = ioloop.IOLoop.instance()
 
-        # fake rabbit doesn't use sockets, use the slow way for it
-        if FLAGS.fake_rabbit:
-            injected = ioloop.PeriodicCallback(
-                lambda: self.fetch(enable_callbacks=True), 0, io_loop=io_inst)
-            injected.start()
-            return injected
-        else:
-            sock = self.connection.connection.transport.sock
-            io_inst.add_handler(sock.fileno(),
-                                self._handle_tornado,
-                                io_inst.READ)
-            return StoppableHandler(sock.fileno(), io_inst)
+        injected = ioloop.PeriodicCallback(
+            lambda: self.fetch(enable_callbacks=True), 1, io_loop=io_inst)
+        injected.start()
+        return injected
 
     attachToTornado = attach_to_tornado
 
-    def _handle_tornado(self, fd, events):
-        self.fetch(enable_callbacks=True)
-    
 
 class Publisher(messaging.Publisher):
     pass
